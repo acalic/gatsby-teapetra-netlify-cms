dd67fa5f6ebca3c843753a1f021c68ad05693594
acalic <andrea.calic@roomex.com>
2019-12-06 12:01:14 +0000

    Fixed insecure content error

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -7,16 +7,16 @@ import './styles.scss';
 
 const images = [
     {
-      original: 'http://lorempixel.com/1000/600/nature/1/',
-      thumbnail: 'http://lorempixel.com/250/150/nature/1/',
+      original: 'https://lorempixel.com/1000/600/nature/1/',
+      thumbnail: 'https://lorempixel.com/250/150/nature/1/',
     },
     {
-      original: 'http://lorempixel.com/1000/600/nature/2/',
-      thumbnail: 'http://lorempixel.com/250/150/nature/2/'
+      original: 'https://lorempixel.com/1000/600/nature/2/',
+      thumbnail: 'https://lorempixel.com/250/150/nature/2/'
     },
     {
-      original: 'http://lorempixel.com/1000/600/nature/3/',
-      thumbnail: 'http://lorempixel.com/250/150/nature/3/'
+      original: 'https://lorempixel.com/1000/600/nature/3/',
+      thumbnail: 'https://lorempixel.com/250/150/nature/3/'
     }
   ]
 
