5f99543630335352fb2eb24f295a3245016cf16b
acalic <andrea.calic@roomex.com>
2020-02-17 14:02:49 +0000

    fix build error featuredpost removed from blog data

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -15,52 +15,6 @@ class BlogRoll extends React.Component {
       <div className="blog columns is-multiline">
         {posts &&
           posts.map(({ node: post }) => (
-            /*<div className="is-parent column is-6" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
-              >
-                <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${
-                            post.title
-                          }`,
-                        }}
-                      />
-                    </div>
-                  ) : null}
-                  <p className="post-meta">
-                    <AniLink
-                      fade
-                      className="title has-text-primary is-size-4"
-                      to={post.fields.slug}
-                    >
-                      {post.frontmatter.title}
-                    </AniLink>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
-                      {post.frontmatter.date}
-                    </span>
-                  </p>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <AniLink
-                    fade
-                    className="button"
-                    to={post.fields.slug}>
-                    Keep Reading
-                  </AniLink>
-                </p>
-              </article>
-            </div>*/
             <article className="is-parent column is-4" key={post.id}>
               <AniLink 
                 fade 
@@ -111,7 +65,6 @@ export default () => (
                 title
                 templateKey
                 date(formatString: "MMMM DD, YYYY")
-                featuredpost
                 featuredimage {
                   childImageSharp {
                     fluid(maxWidth: 400, maxHeight: 300, quality: 100) {
