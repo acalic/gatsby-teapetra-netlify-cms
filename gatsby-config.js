5f41f40897bd16d1e04a74bac123a188174aed81
acalic <acalic1990@gmail.com>
2020-09-03 02:09:33 +0100

    Attempt to fix #2

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,14 +20,14 @@ module.exports = {
         trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
       }
     },
-    {
+    /* {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/static/img`,
         name: 'uploads',
       },
-    },
+    }, */
     {
       resolve: 'gatsby-source-filesystem',
       options: {
