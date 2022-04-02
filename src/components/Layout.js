da05f01b4126d42d6e1b519228eef620b17a4664
acalic <acalic1990@gmail.com>
2022-04-02 02:25:51 +0100

    modified suspension template n2

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,63 +1,8 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-import './all.sass'
-import useSiteMetadata from './SiteMetadata'
+import React from "react";
+import "./all.sass";
 
-import appleTouchIcon from "../img/apple-touch-icon.png"
-import favicon16 from "../img/favicon-16x16.png"
-import favicon32 from "../img/favicon-32x32.png"
+const TemplateWrapper = () => {
+  return <div>site suspended</div>;
+};
 
-import { TinyButton as ScrollUpButton } from "react-scroll-up-button"; //Add this line Here
-
-const TemplateWrapper = ({ children, isHome }) => {
-  const { title, description } = useSiteMetadata()
-
-  return (
-    <div>
-      <Helmet>
-        <html lang="en" />
-        <title>{title}</title>
-        <meta name="description" content={description} />
-
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={appleTouchIcon}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={favicon32}
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={favicon16}
-          sizes="16x16"
-        />
-
-        {/* <link
-          rel="mask-icon"
-          href="/img/safari-pinned-tab.svg"
-          color="#ff4400"
-        /> */}
-
-        <meta name="theme-color" content="#fff" />
-
-        <meta property="og:type" content="business.business" />
-        <meta property="og:title" content={title} />
-        <meta property="og:url" content="/" />
-        <meta property="og:image" content="/img/og-image.jpg" />
-      </Helmet>
-      <Navbar isHome={isHome}/>
-      <div>{children}</div>
-      <Footer />
-      <ScrollUpButton ContainerClassName="btn-scrolltop"/>
-    </div>
-  )
-}
-
-export default TemplateWrapper
+export default TemplateWrapper;
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,55 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
 //import Header from '../components/Header'
-import Content, { HTMLContent } from '../components/Content'
-import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
-import ScrollAnimation from 'react-animate-on-scroll';
+import Content, { HTMLContent } from "../components/Content";
 
-export const AboutPageTemplate = ({ title, mainimage, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
+export const AboutPageTemplate = ({ contentComponent }) => {
+  const PageContent = contentComponent || Content;
 
-  return (
-    <section className="section section--gradient page-about">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="content">
-              <h1 className="title is-size-3 is-bold-light">
-                {title}
-              </h1>
-              <div className="page-about-content">
-                <ScrollAnimation animateIn="slideInLeft">
-                  <div className="about-img-container">
-                    <PreviewCompatibleImage
-                      imageInfo={{
-                        image: mainimage,
-                        alt: `tea petra about me image`,
-                      }}
-                    />
-                  </div>
-                </ScrollAnimation>
-                <ScrollAnimation animateIn="slideInRight">
-                  <PageContent className="content" content={content} />
-                </ScrollAnimation>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
-}
+  return <section className="section section--gradient page-about"></section>;
+};
 
 AboutPageTemplate.propTypes = {
   title: PropTypes.string,
   content: PropTypes.string,
   contentComponent: PropTypes.func,
-}
+};
 
 const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
   return (
     <Layout>
@@ -64,14 +33,14 @@ const AboutPage = ({ data }) => {
         content={post.html}
       />
     </Layout>
-  )
-}
+  );
+};
 
 AboutPage.propTypes = {
   data: PropTypes.object.isRequired,
-}
+};
 
-export default AboutPage
+export default AboutPage;
 
 export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {
@@ -83,4 +52,4 @@ export const aboutPageQuery = graphql`
       }
     }
   }
-`
+`;
